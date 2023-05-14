06bc8b98fa44ddb6c83ca2b7e2252510c04e46b6
ibphantom <62777429+ibphantom@users.noreply.github.com>
2023-05-14 11:09:40 -0400

    Update vote.py

--- scripts/vote.py
+++ scripts/vote.py
@@ -35,62 +35,59 @@ def main():
     if not os.path.exists("FINAL.csv"):
         with open("FINAL.csv", "w", encoding="utf-8") as f:
             f.write("Name,SSN,Hash value,Selection\n")
-    while True:
-    # Prompt the user for input
-           os.system('cls')
-    name = prompt_string("What is your name? ")
-    ssn_last_four = prompt_string("What are the last 4 digits of your SSN? ")   
     
     while True:
         # Prompt the user for input
         os.system('cls')
         name = prompt_string("What is your name? ")
-        os.system('cls')
         ssn_last_four = prompt_string("What are the last 4 digits of your SSN? ")
-        os.system('cls')
-        selection = prompt_choice(
-            "Please select one of the following options: \n"
-            "1. Option 1\n"
-            "2. Option 2\n"
-            "3. Option 3\n"
-            "Your selection: ",
-            1,
-            3,
-        )
         
-        if name == "CHRIS SMITH" and ssn_last_four == "9999"
-        exit()
-        
-        
-        selection_name = {
-            1: "Option 1",
-            2: "Option 2",
-            3: "Option 3",
-        }[selection]
-        os.system('cls')
-        # Hash the user's input
-        hash_value = hashlib.sha256()
-        hash_value.update(name.encode("utf-8"))
-        hash_value.update(ssn_last_four.encode("utf-8"))
-        hash_value.update(selection_name.encode("utf-8"))
-        hash_value = hash_value.hexdigest()
-        
-        # Print the user's selections and ask for confirmation
-        print("You selected:\n")
-        
-        print("Name: {}\n".format(name).center(50))
-        print("SSN: {}\n".format(ssn_last_four).center(50))
-        print("Selection: {}\n\n".format(selection_name).center(50))
-        print("Hash value: {}".format(hash_value).center(50))
-
-        is_correct = prompt_yes_no("Are these selections correct? (Press Y for Yes and N for No) ")
-        
-        
-        print("Your Confirmation Receipt is now Printing")
-        time.sleep(5)
-        
-        print("The values you selected have been hashed/algorithimically combined:")
-        print("Hash value: {}".format(hash_value).center(50))
+        while True:
+            # Prompt the user for input
+            os.system('cls')
+            selection = prompt_choice(
+                "Please select one of the following options: \n"
+                "1. Option 1\n"
+                "2. Option 2\n"
+                "3. Option 3\n"
+                "Your selection: ",
+                1,
+                3,
+            )
+            
+            if name == "CHRIS SMITH" and ssn_last_four == "9999":
+                print("You have entered a restricted name. Exiting script...")
+                exit()
+            
+            selection_name = {
+                1: "Option 1",
+                2: "Option 2",
+                3: "Option 3",
+            }[selection]
+            os.system('cls')
+            # Hash the user's input
+            hash_value = hashlib.sha256()
+            hash_value.update(name.encode("utf-8"))
+            hash_value.update(ssn_last_four.encode("utf-8"))
+            hash_value.update(selection_name.encode("utf-8"))
+            hash_value = hash_value.hexdigest()
+            
+            # Print the user's selections and ask for confirmation
+            print("You selected:\n")
+            
+            print("Name: {}\n".format(name).center(50))
+            print("SSN: {}\n".format(ssn_last_four).center(50))
+            print("Selection: {}\n\n".format(selection_name).center(50))
+            print("Hash value: {}".format(hash_value).center(50))
+            
+            is_correct = prompt_yes_no("Are these selections correct? (Press Y for Yes and N for No) ")
+            
+            
+            print("Your Confirmation Receipt is now Printing")
+            time.sleep(5)
+            
+            print("The values you selected have been hashed/algorithimically combined:")
+            print("Hash value: {}".format(hash_value).center(50))
         # If the selections are correct, write them to a CSV file in the "votes" folder with the user's name and SSN as the filename
         os.system('cls')
 
