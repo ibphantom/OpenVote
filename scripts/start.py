cca9656794415adb2d9ae219d36a1c8d9f0cf897
ibphantom <62777429+ibphantom@users.noreply.github.com>
2023-05-14 19:20:27 -0400

    Update start.py

--- scripts/start.py
+++ scripts/start.py
@@ -1,13 +1,17 @@
 import subprocess
 
-device_type = input("Is this device a server or a client? ")
+try:
+    device_type = input("Is this device a server or a client? ")
+except EOFError:
+    print("Error: End of input reached unexpectedly.")
+    exit(1)
 
 if device_type.lower() == "server":
     print("This device is a server.")
     subprocess.run(["python", "sftp.py"])
 elif device_type.lower() == "client":
     print("This device is a client.")
-    subprocess.run(["python", "host.py"])
+    subprocess.run(["python", "vote.py"])
 else:
     print("Invalid input. Please enter either 'server' or 'client'.")
 
