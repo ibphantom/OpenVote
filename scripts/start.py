d55920f23c94a8548cf0b5dc2884d9bfa0bf1437
ibphantom <62777429+ibphantom@users.noreply.github.com>
2023-05-14 21:35:18 -0400

    Update start.py

--- scripts/start.py
+++ scripts/start.py
@@ -1,6 +1,7 @@
 import subprocess
 import sys
 
+
 while True:
     try:
         device_type = input("Is this device a server or a client? ")
@@ -8,6 +9,15 @@ while True:
     except EOFError:
         print("Error: End of input reached unexpectedly. Please try again.")
 
+# Create a cron job to start vote.py on boot
+cron_job = f'@reboot python3 /vote.py >/dev/null 2>&1\n'
+
+# Open the crontab file for editing and write the cron job
+with open('/etc/crontab', 'a') as file:
+    file.write(cron_job)
+        
+        
+        
 if device_type.lower() == "server":
     print("This device is a server.")
     subprocess.run(["python3", "sftp.py"])
