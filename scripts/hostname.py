08e5c80034fbadaa9da79e3f835041e1bd419a45
ibphantom <62777429+ibphantom@users.noreply.github.com>
2023-05-14 22:02:17 -0400

    Update hostname.py

--- scripts/hostname.py
+++ scripts/hostname.py
@@ -1,23 +1,18 @@
 import subprocess
-import requests
-import random
-import string
+import random_word
 
-# Get a random word from the Wordnik API
-response = requests.get("https://api.wordnik.com/v4/words.json/randomWord?api_key=YOUR_API_KEY")
-if response.status_code != 200:
-    print("Error getting random word")
-    exit()
-random_word = response.json()["word"]
+# Generate a random word
+random_word = random_word.RandomWords().get_random_word()
 
-# Generate a random string of length 8 for suffix
-suffix = ''.join(random.choices(string.ascii_lowercase, k=8))
+# Check if a random word is generated
+if random_word:
+    # Concatenate the hostname prefix and the random word
+    new_hostname = "VoterMachine-" + random_word
 
-# Concatenate the hostname prefix and suffix
-new_hostname = f"VoterMachine-{random_word}-{suffix}"
+    # Rename the hostname
+    subprocess.run(["hostnamectl", "set-hostname", new_hostname])
 
-# Rename the hostname
-subprocess.run(["hostnamectl", "set-hostname", new_hostname])
-
-# Reboot the machine
-subprocess.run(["reboot"])
+    # Reboot the machine
+    subprocess.run(["reboot"])
+else:
+    print("Failed to generate a random word.")
