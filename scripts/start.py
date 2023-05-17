548b518dba304fda6cf503b2f20a9a4df296f2b5
ibphantom <62777429+ibphantom@users.noreply.github.com>
2023-05-16 23:14:46 -0400

    Update start.py

--- scripts/start.py
+++ scripts/start.py
@@ -1,7 +1,8 @@
 import subprocess
 import sys
 import os
-
+import socket
+import uuid
 
 while True:
     try:
@@ -16,14 +17,17 @@ cron_job = f'@reboot python3 /vote.py >/dev/null 2>&1\n'
 # Open the crontab file for editing and write the cron job
 with open('/etc/crontab', 'a') as file:
     file.write(cron_job)
-        
-        
-        
+
 if device_type.lower() == "server":
     print("This device is a server.")
     subprocess.run(["python3", "sftp.py"])
 elif device_type.lower() == "client":
     print("This device is a client.")
+    # Generate a random hostname if one doesn't already exist
+    current_hostname = socket.gethostname()
+    if current_hostname.startswith("localhost"):
+        new_hostname = f"client-{str(uuid.uuid4())[:8]}"
+        subprocess.run(["sudo", "hostnamectl", "set-hostname", new_hostname])
     subprocess.run(["python3", "hostname.py"])
 else:
     print("Invalid input. Please enter either 'server' or 'client'.")
