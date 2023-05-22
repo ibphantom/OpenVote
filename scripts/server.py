82ce910b0976d2dbf2ef460ed990a87df13673be
ibphantom <62777429+ibphantom@users.noreply.github.com>
2023-05-22 19:57:00 -0400

    Update server.py

--- scripts/server.py
+++ scripts/server.py
@@ -1,53 +1,54 @@
 import os.path
 import paramiko
+import getpass
 
+def Update():
+    # Install packages
+    os.system('apt-get install -y openssh-server ufw')
 
+    #Create user
+    #os.system('useradd zach -m -s /bin/bash')
+    #os.system('echo "zach:123456" | chpasswd')
 
-while True:
-        # Install packages
-        os.system('apt-get install -y openssh-server ufw')
-       
-        #Create user
-        #os.system('useradd zach -m -s /bin/bash')
-        #os.system('echo "zach:123456" | chpasswd')
+    # Create /run/sshd directory
+    os.system('mkdir -p /run/sshd')
 
-        # Create /run/sshd directory
-        os.system('mkdir -p /run/sshd')
+    # Set permissions for /run/sshd
+    os.system('chmod 0755 /run/sshd')
 
-        # Set permissions for /run/sshd
-        os.system('chmod 0755 /run/sshd')
-
-        # Start sshd daemon
-        os.system('/usr/sbin/sshd -D')
-
-
-# Define connection parameters
-hostname = '172.16.0.2'
-username = ''
-password = ''
-port = 22
+    # Start sshd daemon
+    os.system('/usr/sbin/sshd -D')
 
 # Define local and remote file paths
 local_file_path = 'C:/file.txt'
 remote_file_path = '/mnt/user/UnraidNAS/ptest/contact.txt'
 
-# Create a new SSH client
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+# Prompt the user for connection parameters
+username = input("Enter username: ")
+password = getpass.getpass("Enter password: ")
+port = 22
 
-# Connect to the server using SSH
-ssh.connect(hostname=hostname, username=username, password=password, port=port)
+# List of hostnames
+hostnames = ["172.16.0.2", "172.16.0.3", "172.16.0.4"]  # replace with your hostnames
 
-# Open an SFTP session over the existing SSH connection
-sftp = ssh.open_sftp()
+for hostname in hostnames:
+    # Create a new SSH client
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-print("Connected to {}".format(hostname))
+    # Connect to the server using SSH
+    ssh.connect(hostname=hostname, username=username, password=password, port=port)
 
-# Transfer the file from local to remote
-sftp.put(local_file_path, remote_file_path)
+    # Open an SFTP session over the existing SSH connection
+    sftp = ssh.open_sftp()
 
-# Close the SFTP session and the SSH connection
-sftp.close()
-ssh.close()
+    print("Connected to {}".format(hostname))
 
-print("File transferred successfully")
+    # Transfer the file from remote to local
+    sftp.get(remote_file_path, local_file_path)  # replacing sftp.put with sftp.get
+
+    # Close the SFTP session and the SSH connection
+    sftp.close()
+    ssh.close()
+
+    print("File transferred successfully from {}".format(hostname))
