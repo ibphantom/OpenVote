54582c4e9eb1b401754d1dfec8a8e2212fd92f91
ibphantom <62777429+ibphantom@users.noreply.github.com>
2023-05-22 23:20:09 -0400

    Update server.py

--- scripts/server.py
+++ scripts/server.py
@@ -18,10 +18,14 @@ def check_hosts(hostnames, username, password, port):
             ssh.close()
         except Exception as e:
             print("Failed to connect via SSH to ", host, "due to: ", e)
-            
+
 def Update():
     # Your existing code here
     pass
+
+# Ask for a list of hostnames
+hosts_input = input("Enter a list of hostnames separated by commas: ")
+hostnames = [host.strip() for host in hosts_input.split(',')]
 
 # Define local and remote file paths
 local_file_path = 'C:/file.txt'
@@ -31,10 +35,6 @@ remote_file_path = '/mnt/user/UnraidNAS/ptest/contact.txt'
 username = input("Enter username: ")
 password = getpass.getpass("Enter password: ")
 port = 22
-
-# Ask for a list of hostnames
-hosts_input = input("Enter a list of hostnames separated by commas: ")
-hostnames = [host.strip() for host in hosts_input.split(',')]
 
 # Checking hosts before starting the main script
 check_hosts(hostnames, username, password, port)
