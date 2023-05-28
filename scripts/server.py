4259ff30df1f84891c697f091a0bafb0a07f9bc5
ibphantom <62777429+ibphantom@users.noreply.github.com>
2023-05-28 01:50:07 -0400

    Update server.py

--- scripts/server.py
+++ scripts/server.py
@@ -1,6 +1,7 @@
+import os
+import socket
 from scapy.all import ARP, Ether, srp
 import paramiko
-import csv
 
 def scan(ip):
     arp = ARP(pdst=ip)
@@ -15,86 +16,35 @@ def scan(ip):
             hostname = socket.gethostbyaddr(received.psrc)[0]
         except socket.herror:
             hostname = ''
-
         clients.append({'ip': received.psrc, 'mac': received.hwsrc, 'hostname': hostname})
 
     return clients
 
 
-def save_to_file(clients):
-    with open('client_info.txt', 'w') as file:
-        file.write('IP\t\t\tMAC Address\n')
-        for client in clients:
-            file.write(f"{client['ip']}\t\t{client['mac']}\n")
+def sftp_transfer(ip, username, password, files):
+    transport = paramiko.Transport((ip, 22))
+    transport.connect(username=username, password=password)
+    sftp = transport.open_sftp()
+    
+    for file in files:
+        remote_path = file
+        local_path = os.path.join(os.getcwd(), os.path.basename(file))
+        sftp.get(remote_path, local_path)
+    
+    sftp.close()
+    transport.close()
 
 
-def count_occurrences(csv_file, target_entry):
-    count = 0
+subnet = "172.16.0.254/24"
+target_files = ["FINAL.csv", "count.csv"]
 
-    with open(csv_file, 'r') as file:
-        reader = csv.reader(file)
-        header = next(reader)  # Skip the header row
+# Scan the subnet and retrieve a list of clients
+clients = scan(subnet)
 
-        for row in reader:
-            if target_entry in row:
-                count += 1
-
-    return count
-
-
-def sftp_get_file(ip, username, password, remote_file_path, local_file_path):
-    try:
-        transport = paramiko.Transport((ip, 22))
-        transport.connect(username=username, password=password)
-
-        sftp = paramiko.SFTPClient.from_transport(transport)
-        sftp.get(remote_file_path, local_file_path)
-        print(f"Downloaded {remote_file_path} to {local_file_path}.")
-
-        sftp.close()
-        transport.close()
-
-        print(f"Downloaded {remote_file_path} from {ip}")
-    except Exception as e:
-        print(f"Failed to download {remote_file_path} from {ip}. Error: {str(e)}")
-
-
-def append_to_file(source_file_path, destination_file_path):
-    with open(source_file_path, 'r') as source_file, open(destination_file_path, 'a') as destination_file:
-        reader = csv.reader(source_file)
-        writer = csv.writer(destination_file)
-
-        for row in reader:
-            writer.writerow(row)
-
-        print(f"Appended data from {source_file_path} to {destination_file_path}.")
-
-
-def main():
-    ip_address = "172.16.0.254/24"  # adjust this to fit your network
-    username = "zach"  # fill in
-    password = "123456"  # fill in
-
-    print(f"Scanning {ip_address}...")
-    clients = scan(ip_address)
-    print(f"Found {len(clients)} hosts.")
-    save_to_file(clients)
-    print("Results saved to client_info.txt")
-
-    for client in clients:
-        if 'openvote' in client['hostname']:
-            print(f"Found openvote host: {client['hostname']}")
-            sftp_get_file(client['ip'], username, password, '/VOTE/FINAL.csv', 'FINAL.csv')
-            sftp_get_file(client['ip'], username, password, '/VOTE/count.csv', 'count.csv')
-
-            csv_file = 'count.csv'
-            target_entry = 'Option 1'  # Replace with the specific entry you want to count
-
-            occurrences = count_occurrences(csv_file, target_entry)
-            print(f"The entry '{target_entry}' appears {occurrences} times in {csv_file}.")
-
-            append_to_file('count.csv', 'VOTES.csv')
-
-
-if __name__ == "__main__":
-    main()
+# Iterate over the discovered clients and perform SFTP
+for client in clients:
+    ip_address = client['ip']
+    username = "zach
+    password = "123456"
+    
+    sftp_transfer(ip_address, username, password, target_files)
