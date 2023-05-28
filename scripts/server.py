f5d40c56e714f02e32abf5e324d87e50eb594194
ibphantom <62777429+ibphantom@users.noreply.github.com>
2023-05-28 02:28:54 -0400

    Update server.py

--- scripts/server.py
+++ scripts/server.py
@@ -1,7 +1,7 @@
-import os
-import socket
 from scapy.all import ARP, Ether, srp
 import paramiko
+import socket
+import csv
 
 def scan(ip):
     arp = ARP(pdst=ip)
@@ -14,39 +14,61 @@ def scan(ip):
     for sent, received in result:
         try:
             hostname = socket.gethostbyaddr(received.psrc)[0]
-        except socket.herror:
+        except socket.herror:  # unable to resolve
             hostname = ''
         
-        if 'openvote' in hostname.lower():
-            clients.append({'ip': received.psrc, 'mac': received.hwsrc, 'hostname': hostname})
+        clients.append({'ip': received.psrc, 'mac': received.hwsrc, 'hostname': hostname})
 
     return clients
 
 
-def sftp_transfer(ip, username, password, files):
-    transport = paramiko.Transport((ip, 22))
-    transport.connect(username=username, password=password)
-    sftp = transport.open_sftp()
-    
-    for file in files:
-        remote_path = file
-        local_path = os.path.join(os.getcwd(), os.path.basename(file))
-        sftp.get(remote_path, local_path)
-    
-    sftp.close()
-    transport.close()
+def save_to_file(clients):
+    with open('client_info.txt', 'w') as file:
+        file.write('IP\t\t\tMAC Address\t\tHostname\n')
+        for client in clients:
+            file.write(f"{client['ip']}\t\t{client['mac']}\t\t{client['hostname']}\n")
 
 
-subnet = "172.16.0.254/24"
-target_files = ["FINAL.csv" , "count.csv"]
+def sftp_get_file(ip, username, password, remote_file_path, local_file_path):
+    try:
+        transport = paramiko.Transport((ip, 22))
+        transport.connect(username=username, password=password)
 
-# Scan the subnet and retrieve a list of clients with hostname containing "openvote"
-clients = scan(subnet)
+        sftp = paramiko.SFTPClient.from_transport(transport)
+        sftp.get(remote_file_path, local_file_path)
+        print(f"Downloaded {remote_file_path} to {local_file_path}.")
 
-# Iterate over the discovered clients and perform SFTP
-for client in clients:
-    ip_address = client['ip']
-    username = "zach"
-    password = "123456"
-    
-    sftp_transfer(ip_address, username, password, target_files)
+        # Read the content of the downloaded file and append it to VOTES.csv
+        with open(local_file_path, 'r') as source_file:
+            reader = csv.reader(source_file)
+            data = list(reader)
+            print(f"Read {len(data)} rows from {local_file_path}.")
+            with open('VOTES.csv', 'a') as destination_file:
+                writer = csv.writer(destination_file)
+                for row in data:
+                    writer.writerow(row + [ip])
+                print(f"Appended data to VOTES.csv.")
+
+        sftp.close()
+        transport.close()
+
+        print(f"Downloaded and appended {remote_file_path} from {ip}")
+    except Exception as e:
+        print(f"Failed to download and append {remote_file_path} from {ip}. Error: {str(e)}")
+
+
+if __name__ == "__main__":
+    ip_address = "172.16.0.254/24"  # adjust this to fit your network
+    username = "zach"  # fill in
+    password = "123456"  # fill in
+
+    print(f"Scanning {ip_address}...")
+    clients = scan(ip_address)
+    print(f"Found {len(clients)} hosts.")
+    save_to_file(clients)
+    print("Results saved to client_info.txt")
+
+    for client in clients:
+        if 'openvote' in client['hostname']:
+            print(f"Found openvote host: {client['hostname']}")
+            sftp_get_file(client['ip'], username, password, '/VOTE/FINAL.csv', f"{client['hostname']}_FINAL.csv")
