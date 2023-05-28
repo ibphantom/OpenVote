9b7da8a87671725165d02cdceb72e973953f6537
ibphantom <62777429+ibphantom@users.noreply.github.com>
2023-05-28 01:28:51 -0400

    Update server.py

--- scripts/server.py
+++ scripts/server.py
@@ -1,6 +1,5 @@
 from scapy.all import ARP, Ether, srp
 import paramiko
-import socket
 import csv
 
 def scan(ip):
@@ -12,21 +11,16 @@ def scan(ip):
     clients = []
 
     for sent, received in result:
-        try:
-            hostname = socket.gethostbyaddr(received.psrc)[0]
-        except socket.herror:  # unable to resolve
-            hostname = ''
-
-        clients.append({'ip': received.psrc, 'mac': received.hwsrc, 'hostname': hostname})
+        clients.append({'ip': received.psrc, 'mac': received.hwsrc})
 
     return clients
 
 
 def save_to_file(clients):
     with open('client_info.txt', 'w') as file:
-        file.write('IP\t\t\tMAC Address\t\tHostname\n')
+        file.write('IP\t\t\tMAC Address\n')
         for client in clients:
-            file.write(f"{client['ip']}\t\t{client['mac']}\t\t{client['hostname']}\n")
+            file.write(f"{client['ip']}\t\t{client['mac']}\n")
 
 
 def count_occurrences(csv_file, target_entry):
@@ -60,6 +54,17 @@ def sftp_get_file(ip, username, password, remote_file_path, local_file_path):
         print(f"Failed to download {remote_file_path} from {ip}. Error: {str(e)}")
 
 
+def append_to_file(source_file_path, destination_file_path):
+    with open(source_file_path, 'r') as source_file, open(destination_file_path, 'a') as destination_file:
+        reader = csv.reader(source_file)
+        writer = csv.writer(destination_file)
+
+        for row in reader:
+            writer.writerow(row)
+
+        print(f"Appended data from {source_file_path} to {destination_file_path}.")
+
+
 def main():
     ip_address = "172.16.0.254/24"  # adjust this to fit your network
     username = "zach"  # fill in
@@ -74,14 +79,16 @@ def main():
     for client in clients:
         if 'openvote' in client['hostname']:
             print(f"Found openvote host: {client['hostname']}")
-            sftp_get_file(client['ip'], username, password, '/VOTE/FINAL.csv', f"/VOTE/FINAL.csv")
-            sftp_get_file(client['ip'], username, password, '/VOTE/count.csv', f"/VOTE/count.csv")
+            sftp_get_file(client['ip'], username, password, '/VOTE/FINAL.csv', 'FINAL.csv')
+            sftp_get_file(client['ip'], username, password, '/VOTE/count.csv', 'count.csv')
 
-            csv_file = f"{client['hostname']}_count.csv"
+            csv_file = 'count.csv'
             target_entry = 'Option 1'  # Replace with the specific entry you want to count
 
             occurrences = count_occurrences(csv_file, target_entry)
             print(f"The entry '{target_entry}' appears {occurrences} times in {csv_file}.")
+
+            append_to_file('count.csv', 'VOTES.csv')
 
 
 if __name__ == "__main__":
